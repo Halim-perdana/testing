76fa61173096555525a8d56caa92b71e7df19c76
halimperdana <elmusys@gmail.com>
2017-01-06 17:28:16 +0700

    Add files via upload

---
+++ UAS S1/BukuAlamatGUI/src/ptik/Statement.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ptik;
+
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author Mr.0
+ */
+class Statement {
+
+    ResultSet executeQuery(String select__from_alamat) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
